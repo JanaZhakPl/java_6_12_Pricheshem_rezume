2674bae6e60f38609965715d2acbab56ecf7a80d
JanaZhakPl <janazhakpl@gmail.com>
2021-10-25 10:15:41 +0200

    WITH Locale.ROOT

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -10,9 +10,9 @@ public class Main {
         String career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
 
         //напишите тут ваш код
-        System.out.println("RESUME".toLowerCase(Locale.ROOT));
-        System.out.println(("TITLE: " + title).toLowerCase(Locale.ROOT));
-        System.out.println(("DEGREE: " + degree).toLowerCase(Locale.ROOT));
-        System.out.println(("CAREER: " + career).toLowerCase(Locale.ROOT));
+        System.out.println("RESUME".toLowerCase());
+        System.out.println(("TITLE: " + title).toLowerCase());
+        System.out.println(("DEGREE: " + degree).toLowerCase());
+        System.out.println(("CAREER: " + career).toLowerCase());
     }
 }
